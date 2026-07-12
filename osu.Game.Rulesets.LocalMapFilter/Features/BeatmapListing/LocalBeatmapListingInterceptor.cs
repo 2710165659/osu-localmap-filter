118464d6191cc81910dc33610817c6b6b5d9968f
2710165659 <2710165659@qq.com>
2026-07-12 14:05:51 +0800

    fix: 修复功能失效问题

--- osu.Game.Rulesets.LocalMapFilter/Features/BeatmapListing/LocalBeatmapListingInterceptor.cs
+++ osu.Game.Rulesets.LocalMapFilter/Features/BeatmapListing/LocalBeatmapListingInterceptor.cs
@@ -12,7 +12,6 @@ using osu.Game.Beatmaps;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Overlays;
 using osu.Game.Overlays.BeatmapListing;
-using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Overlays.Toolbar;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.LocalMapFilter.Features.Injection;
@@ -304,7 +303,7 @@ public partial class LocalBeatmapListingInterceptor : AbstractHandler
 
             if (!cache.TryGetValue(beatmapSet.OnlineID, out bool isLocal))
             {
-                isLocal = beatmapManager!.IsAvailableLocally(new APIBeatmap { OnlineID = beatmapSet.OnlineID });
+                isLocal = beatmapSet.Beatmaps.Any(beatmapManager!.IsAvailableLocally);
                 cache[beatmapSet.OnlineID] = isLocal;
             }
 
